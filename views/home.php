89cbd1104da2add4a60bcb71b7fd642e94697e79
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-07-20 13:26:03 -0400

    "Added FileUploader class to autoload, updated home view with Bootstrap CSS and JS, and refactored product list display"

--- views/home.php
+++ views/home.php
@@ -11,6 +11,8 @@
     <link rel="stylesheet" href="assets/css/estilos.css">
     <!-- jquery cdn -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <!-- CSS Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
@@ -32,46 +34,31 @@
 
     <main class="main_section">
         <div class="form_container">
-            <h1>Listado de productos y Categoría</h1>
-            <div class="lista_productos">
-                <table>
-                    <tr>
-                        <th>Producto</th>
-                        <th>Descripción</th>
-                        <th>Precio</th>
-                        <th>Categoría</th>
-                    </tr>
-                    <?php foreach ($productos as $producto) { ?>
-                        <tr>
-                            <td>
-                                <div class="celda">
-                                    <?= $producto["nombre"] ?>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="celda">
-                                    <?= $producto["descripcion"] ?>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="celda">
-                                    <?= $producto["precio"] ?>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="celda">
-                                    <?= $producto["categoria_nombre"] ?>
-                                </div>
-                            </td>
-                        </tr>
-                    <?php } ?>
-                </table>
+            <h1 class="fs-3 fw-normal">Encuentra aquí</h1>
+            <h1 class="fs-2 text-decoration-underline">VINILOS NUEVOS</h1>
+            <div class="lista_productos row mt-3 py-5">
+                <?php foreach ($productos as $producto) { ?>
+                    <div class="col-12 col-md-3 d-flex justify-content-around">
+                        <div class="card mb-5 w-75">
+                            <img src="backend/<?= $producto["imagen"]  ?>" class="card-img-top" alt="Imagen del producto">
+                            <div class="card-body">
+                                <p class="card-text fs-5 fw-bold"> <?= $producto["nombre"] ?></p>
+                                <p class="card-text">Condición: <?= $producto["descripcion"] ?></p>
+                                <p class="card-text">Valor: <?= $producto["precio"] ?></p>
+                                <p class="card-text">Género: <?= $producto["categoria_nombre"] ?></p>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
             </div>
         </div>
     </main>
     <footer id="productos_footer">
         <p>Hecho por - Miguel Ángel Monzón Martínez</p>
     </footer>
+
+    <!-- JS Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
