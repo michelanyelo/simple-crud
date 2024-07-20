c076182ae621475a0533fe3c998144d53009a7a8
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-07-20 13:30:39 -0400

    "Added FileUploader class to autoload, updated home view with Bootstrap CSS and JS, and refactored product list display"

--- class/autoload.php
+++ class/autoload.php
@@ -4,7 +4,8 @@
 $classMap = [
     'Database' => __DIR__ . '/database.php',
     'Categoria' => __DIR__ . '/categorias.php',
-    'Producto' => __DIR__ . '/productos.php'
+    'Producto' => __DIR__ . '/productos.php',
+    'FileUploader' => __DIR__ . '/fileuploader.php'
 ];
 
 spl_autoload_register(function ($class_name) use ($classMap) {
